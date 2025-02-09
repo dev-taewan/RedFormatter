a726044f3bfbf1f0e20c08f9aa9e33bf1654a37f
dev-taewan <root0607@gmail.com>
2025-02-09 20:34:05 +0900

    conan init

--- IssueList.h
+++ IssueList.h
@@ -2,30 +2,30 @@
 #include <QAbstractlistModel>
 #include <QQmlEngine>
 
-// class IssueItem{
-// public:
-//     QString name;
-//     QString description;
-// };
+class IssueItem{
+public:
+    QString name;
+    QString description;
+};
 
-// class IssueList : public QAbstractListModel
-// {
-//     Q_OBJECT
-//     QML_ELEMENT
-// public:
-//     enum ItemRoles{
-//         NameRole=Qt::UserRole+1,
-//         DescriptionRole,
+class IssueList : public QAbstractListModel
+{
+    Q_OBJECT
+    QML_ELEMENT
+public:
+    enum ItemRoles{
+        NameRole=Qt::UserRole+1,
+        DescriptionRole,
 
-//     };
-//     explicit IssueList(QObject *parent = nullptr);
-//     int rowCount(const QModelIndex &parent=QModelIndex())const override;
-//     QVariant data(const QModelIndex &index,int role=Qt::DisplayRole) const override;
-//     Q_INVOKABLE void addItem(const QString &name,const QString &description);
-//     Q_INVOKABLE void fetch_issues();
-// protected:
-//     QHash<int,QByteArray> roleNames() const override;
-// private:
-//     QList<IssueItem> m_items;
-// };
+    };
+    explicit IssueList(QObject *parent = nullptr);
+    int rowCount(const QModelIndex &parent=QModelIndex())const override;
+    QVariant data(const QModelIndex &index,int role=Qt::DisplayRole) const override;
+    Q_INVOKABLE void addItem(const QString &name,const QString &description);
+    Q_INVOKABLE void fetch_issues();
+protected:
+    QHash<int,QByteArray> roleNames() const override;
+private:
+    QList<IssueItem> m_items;
+};
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
 
 int main(int argc, char *argv[])
 {
